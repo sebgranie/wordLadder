e698f33b44ee949ae4c031ed403b207573d3f354
sebgranie <granie.sebastien7@gmail.com>
2022-11-12 11:07:32 +0000

    clean + delete AdjListNode

--- wordladder/Main.java
+++ wordladder/Main.java
@@ -19,24 +19,18 @@ public class Main {
 
 		// read in the data here
 		List<String> myWords = new ArrayList<String>();
-		// int index = 0;
 		boolean status = false;
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
 			myWords.add(line);
-			// index++;
 		}
+		reader.close();
 
-		// int initial_array_size = myWords.size();
 		System.out.println("Size of the dictionary :" + myWords.size());
 		System.out.println("BeginWord :" + beginWord);
 		System.out.println("EndWord :" + endWord);
 
-		/*
-		 * If the target word does not exist - Possible to delete these lines as it is
-		 * mentioned in the coursework that beginWord and endWord will be in the
-		 * dictionnary.
-		 */
+		// If the target word does not exist - Lines to build a robust program.
 		if (!myWords.contains(beginWord))
 			throw new RuntimeException(beginWord + " is not in word list");
 		if (!myWords.contains(endWord))
@@ -45,34 +39,7 @@ public class Main {
 		// create graph here
 		Graph G = new Graph(myWords);
 
-		// G.display_vertex();
-
-		reader.close();
-
-		// do the work here
-
-		/*
-		 * One fois que le graphe est crée et qu'il est constitué de tableaux de
-		 * tableaux
-		 * En realité le tableau principal comporte tous les noeuds (mots)
-		 * Tous les sous-tableaux constituent tous les liens que chaque noeud possède
-		 *
-		 * On initialise une : Queue<String> queue = new LinkedList<String>()
-		 * On y ajoute le Beginword : queue.offer(beginWord)
-		 * declare un integer correspondant à la distance entre beginword et endword
-		 * (qui sera incrémenté à chaque tour d'itération de la queue)
-		 * Tant que la queue n'est pas vide :
-		 * On itère sur ses éléments :
-		 * On enlève le mot en tête de queue
-		 * On itère sur ses voisins de graphes --> sur sa ajdList
-		 * Recursion jusqu'à que le endword soit présent dans une des linkedList de
-		 * suite de voisins ??
-		 * (a chaque étape, on visite les voisins des voisins en accédant aux linkedList
-		 *
-		 *
-		 */
-
-		// Initialisation
+		// Initialisation of the queue to perform BFS
 		Queue<Vertex> queue = new LinkedList<Vertex>();
 		queue.offer(new Vertex(beginWord));
 		int level = 1;
@@ -80,15 +47,11 @@ public class Main {
 		// Iterate over the queue as long as it is not empty
 		outerloop: while (!queue.isEmpty()) {
 			int size = queue.size();
-			// for (Vertex s : queue)
-			// System.out.println("queue :" + s.word + " of size : " + size);
 			for (int i = 0; i < size; i++) {
 				Vertex current_vertex = queue.poll();
-				// System.out.println("current_vertex:" + current_vertex.word);
 				for (Vertex vertex_inList : G.getVertex(myWords.indexOf(current_vertex.word)).getAdjList()) {
 					if (vertex_inList.visited)
 						continue;
-					// System.out.println("vertex_inList : " + vertex_inList.word);
 					vertex_inList.setPredecessor(current_vertex);
 					if (!vertex_inList.visited && vertex_inList.word.equals(endWord)) {
 						level++;
@@ -116,10 +79,8 @@ public class Main {
 			level = 0;
 			System.out.println("No ladder possible between " + beginWord + " and " + endWord);
 		}
-
 		// end timer and print total time
 		long end = System.currentTimeMillis();
 		System.out.println("\nElapsed time: " + (end - start) + " milliseconds");
-		// return 0;
 	}
 }
--- wordladder/Vertex.java
+++ wordladder/Vertex.java
@@ -1,40 +1,19 @@
 import java.util.LinkedList;
 
-/**
- * class to represent a vertex in a graph
- */
+// class to represent a vertex in a graph
 public class Vertex {
 
     private LinkedList<Vertex> adjList; // the adjacency list
     private int index; // the index of this vertex in the graph
-    public String word;
-
-    // possibly other fields, for example representing data
-    // stored at the node, whether the vertex has been visited
-    // in a traversal, its predecessor in such a traversal, etc.
-
+    public String word; // name of the Vertex
     boolean visited; // whether vertex has been visited in a traversal
     Vertex predecessor; // index of predecessor vertex in a traversal
 
-    /**
-     * creates a new instance of Vertex
-     */
+    // creates a new instance of Vertex
     public Vertex(String new_word) {
         adjList = new LinkedList<Vertex>();
-        // int index = n;
         word = new_word;
-        // System.out.println("word :" + word);
     }
-
-    // /**
-    // * copy constructor
-    // */
-    // public Vertex(Vertex v) {
-    // adjList = v.getAdjList();
-    // index = v.getIndex();
-    // visited = v.getVisited();
-
-    // }
 
     public LinkedList<Vertex> getAdjList() {
         return adjList;
@@ -73,7 +52,6 @@ public class Vertex {
     }
 
     public void addToAdjList(Vertex newVertex) {
-        // adjList.add(new AdjListNode(newVertex));
         adjList.add(newVertex);
     }
 
@@ -83,13 +61,10 @@ public class Vertex {
 
     public int distance(Vertex neighbour) {
         int diff = 0;
-        // System.out.println(neighbour.word);
-        // System.out.println(this.word);
         for (int i = 0; i < neighbour.word.length(); i++) {
             if (neighbour.word.charAt(i) != word.charAt(i))
                 diff++;
         }
         return diff;
     }
-
 }
